a10081ac99c2253dd2050dc166f3ff42e1aa5d11
Tianyi Wang <wang103uiuc@gmail.com>
2012-11-08 20:47:23 -0600

    Completed basic tutorial

--- Experimental/src/edu/uiuc/tianyiwang/locationdetector/MainActivity.java
+++ Experimental/src/edu/uiuc/tianyiwang/locationdetector/MainActivity.java
@@ -2,10 +2,14 @@ package edu.uiuc.tianyiwang.locationdetector;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends Activity {
-
+	public final static String EXTRA_MESSAGE = "edu.uiuc.tianyi.locationdetector.MESSAGE";
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,5 +20,13 @@ public class MainActivity extends Activity {
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.activity_main, menu);
         return true;
+    }
+    
+    public void sendMessage(View view) {
+    	Intent intent = new Intent(this, DisplayMessageActivity.class);
+    	EditText editText = (EditText) findViewById(R.id.edit_message);
+    	String message = editText.getText().toString();
+    	intent.putExtra(EXTRA_MESSAGE, message);
+    	startActivity(intent);
     }
 }
