0a4fef2ada67126f2432670aa7044b847f4f359c
Tianyi Wang <wang103uiuc@gmail.com>
2012-11-16 00:56:51 -0600

    Minor

--- Experimental/src/edu/uiuc/tianyiwang/locationdetector/DisplayMessageActivity.java
+++ Experimental/src/edu/uiuc/tianyiwang/locationdetector/DisplayMessageActivity.java
@@ -1,6 +1,5 @@
 package edu.uiuc.tianyiwang.locationdetector;
 
-import android.net.nsd.NsdServiceInfo;
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
@@ -12,10 +11,7 @@ import android.support.v4.app.NavUtils;
 public class DisplayMessageActivity extends Activity {
 
 	public void registerService(int port) {
-		NsdServiceInfo serviceInfo = new NsdServiceInfo();
-		serviceInfo.setServiceName("DigitalStickyNotesService");
-		serviceInfo.setServiceType("_http._tcp");
-		serviceInfo.setPort(port);		
+		// Only available in API 16 and higher.
 	}
 	
     @Override
