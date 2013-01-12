8d9d5afacf681ea1820f3cb577d4638467e501c3
Tianyi Wang <wang103uiuc@gmail.com>
2013-01-11 22:42:34 -0600

    minor

--- ServerCode/src/edu/illinois/bluetooth/ProcessConnectionThread.java
+++ ServerCode/src/edu/illinois/bluetooth/ProcessConnectionThread.java
@@ -1,15 +1,31 @@
 package edu.illinois.bluetooth;
 
+import java.io.IOException;
+import java.io.InputStream;
+
 import javax.microedition.io.StreamConnection;
 
 public class ProcessConnectionThread implements Runnable {
 
+	final static private int MAX_MSG_LENGTH = 1024;
+	
+	private StreamConnection streamConnection;
+	
 	public ProcessConnectionThread(StreamConnection streamConnection) {
-		
+		this.streamConnection = streamConnection;
 	}
 
 	@Override
 	public void run() {
-		
+		try {
+			InputStream inputStream = streamConnection.openInputStream();
+			
+			byte message[] = new byte[MAX_MSG_LENGTH];
+			inputStream.read(message);
+			
+			System.out.println(message);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 }
