b88fb47c7425b567c3aa33ee9325a09b374583b8
Tianyi Wang <wang103uiuc@gmail.com>
2013-01-30 17:29:52 -0600

    minor

--- Code/src/edu/illinois/communication/Communicator.java
+++ Code/src/edu/illinois/communication/Communicator.java
@@ -1,9 +1,28 @@
 package edu.illinois.communication;
 
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+
 import edu.illinois.classinterfaces.ConnectionManager;
 import edu.illinois.digitalstickynotes.MainActivity;
 
 public class Communicator {
+	
+	private static final String urlAddress = "http://tianyiwang.info/dsn/handle_requests.php";
+	
+	private HttpClient httpClient;
+	private HttpPost httpPost;
 	
 	@SuppressWarnings("unused")			//TODO: remove this.
 	private ConnectionManager connectionManager;
@@ -12,11 +31,34 @@ public class Communicator {
 		//TODO: now only try to authenticate against the main database, need to add
 		// support for authentication against local server.
 		
+		List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(2);
+		nameValuePairs.add(new BasicNameValuePair("email", email));
+		nameValuePairs.add(new BasicNameValuePair("pwd", password));
+		
+		try {
+			httpPost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		}
+		
+		HttpResponse response = null;
+		try {
+			response = httpClient.execute(httpPost);
+		} catch (ClientProtocolException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		//TODO: look at response.
 		
 		return true;
 	}
 	
 	public Communicator() {
+		httpClient = new DefaultHttpClient();
+		httpPost = new HttpPost(urlAddress);
+		
 		this.connectionManager = MainActivity.connectionManager;
 	}
 }
--- PHP/handle_requests.php
+++ PHP/handle_requests.php
@@ -1,7 +1,35 @@
 <?php
 session_start();
 
+$con = mysql_connect('68.178.137.113', 'dsntianyi', 'Digital!1');
+if (!$con) {
+    die('Could not connect: ' . mysql_error());
+}
+mysql_select_db('dsntianyi', $con);
+
 $request_name = $_POST['request_name'];
 
-echo 'something';
+# Handle request.
+if ($request_name === 'authenticate') {
+    # Escape special characters.
+    if (!get_magic_quotess_gpc()) {
+        $email = mysql_real_escape_string($_POST['email']);
+    } else {
+        $email = $_POST['email'];
+    }
+
+    # Check against the DB.
+    $qry = "SELECT user_name FROM users WHERE email='" . $email . "' AND password='" . md5($_POST['pwd']) . "'";
+    $qry_result = mysql_query($qry);
+
+    if (mysql_num_rows($qry_result) > 0) {
+        echo 'Sign in success';       
+    } else {
+        echo 'Sign in failed';
+    }
+} else {
+    echo 'Invalid Request';
+}
+
+mysql_close($con);
 ?>
