c9144cc9401f9e70e228fc8bb5ac437f970ea1a0
Tianyi Wang <wang103uiuc@gmail.com>
2013-02-05 23:57:28 -0600

    global server register done

--- PHP/handle_requests.php
+++ PHP/handle_requests.php
@@ -27,7 +27,47 @@ if ($request_name === 'authenticate') {
     } else {
         $result = array('success' => false, 'info' => 'User does not exist.');
     }
-} else {
+}
+elseif ($request_name == 'register') {
+    # Escape special characters.
+    if (!get_magic_quotes_gpc()) {
+        $email = mysql_real_escape_string($_POST['email']);
+        $usrname = mysql_real_escape_string($_POST['username']);
+    } else {
+        $email = $_POST['email'];
+        $usrname = $_POST['username'];
+    }
+    $firstname = $_POST['firstname'];
+    $lastname = $_POST['lastname'];
+    $pwd = md5($_POST['pwd']);
+
+    # Check against the DB.
+    $qry = "SELECT user_name FROM users WHERE email='" . $email . "'";
+    $qry_result = mysql_query($qry);
+    if (mysql_num_rows($qry_result) > 0) {
+        $result = array('code' => 1, 'info' => 'Email exists.');
+        mysql_close($con);
+        echo json_encode($result);
+        die();
+    }
+
+    $qry = "SELECT user_name FROM users WHERE user_name='" . $usrname . "'";
+    $qry_result = mysql_query($qry);
+    if (mysql_num_rows($qry_result) > 0) {
+        $result = array('code' => 2, 'info' => 'Username exists.');
+        mysql_close($con);
+        echo json_encode($result);
+        die();
+    }
+
+    # Insert new user into the DB.
+    $qry = 'INSERT INTO users (user_name, password, first_name, last_name, email) VALUES ("' . $usrname . '", "' . $pwd . '", "' . $firstname . '", "' . $lastname . '", "' . $email . '")';
+    if (!mysql_query($qry, $con)) {
+        die('Error: ' . mysql_error());
+    }
+    $result = array('code' => 0, 'info' => 'Register successful.');
+}
+else {
     $result = array('success' => false, 'info' => 'Request Not Supported!');
 }
 
