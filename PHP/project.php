04be3aeeba996c2be0624d1950c870319f31b17c
Tianyi Wang <wang103uiuc@gmail.com>
2013-02-08 05:02:57 -0600

    Started the consumer stuffs

---
+++ PHP/project.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Digital Sticky Notes Project</title>
+    </head>
+    
+    <body>
+        
+        <h1>
+            This is the homepage for the Digital Sticky Notes project.
+        </h1>
+
+        <?php
+        echo '
+        <p>To request for consumer key and secret, please contact the project owner.</p>
+        <form name="consumer_form" action="consumer.php" method="post">
+        <p>
+        <label>Code:</label> <input type="text" name="code" required>
+        </p>
+        <p>
+        <input type="submit" value="Submit">
+        </p>
+        </form>';
+        ?>
+
+    </body>
+
+</html>
