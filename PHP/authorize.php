5052fd1eb4dfbd60d4dd8ad5e3f546a15050dc10
Tianyi Wang <wang103uiuc@gmail.com>
2013-02-16 18:04:50 -0600

    Maybe done

---
+++ PHP/authorize.php
@@ -0,0 +1,28 @@
+<?php
+
+require "lib/server/MySQLOAuth2.inc";
+
+$oauth = new MySQLOAuth2();
+
+if ($_POST) {
+  $oauth->finishClientAuthorization($_POST["accept"] == "Yes", $_POST);
+}
+
+$auth_params = $oauth->getAuthorizeParams();
+
+?>
+<html>
+  <head>Authorize The Application</head>
+  <body>
+    <form method="post" action="authorize.php">
+      <?php foreach ($auth_params as $k => $v) { ?>
+      <input type="hidden" name="<?php echo $k ?>" value="<?php echo $v ?>" />
+      <?php } ?>
+      Do you authorize the app to access your data for DigitalStickyNotes?
+      <p>
+        <input type="submit" name="accept" value="Yes" />
+        <input type="submit" name="accept" value="No" />
+      </p>
+    </form>
+  </body>
+</html>
