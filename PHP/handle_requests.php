e33bfd4c55be5cf031679174cc3ba8fc88b0b066
Tianyi Wang <wang103uiuc@gmail.com>
2013-02-28 02:50:03 -0600

    Added username to the access token part

--- PHP/handle_requests.php
+++ PHP/handle_requests.php
@@ -12,12 +12,23 @@ mysql_select_db('dsntianyi', $con);
 
 # Before handling any requests, check access token first.
 $oauth = new MySQLAuth2();
-$oauth->verifyAccessToken();
+if ($oauth->verifyAccessToken() != true) {
+    $result = array('success' => false, 'info' => 'Invalid access token!');
+    mysql_close($con);
+    echo json_encode($result);
+    exit;
+}
 
 $request_name = $_POST['request_name'];
 
 # Handle request.
 if ($request_name === 'get_notes') {
+    $location_id = $_POST['location_id'];
+    $server_pwd = md5($_POST['server_pwd']);
+    $user_name = $oauth->getUsername();
+
+
+
     $result = array('success' => false);
 }
 else {
